1b4276c87a15bef5d274e539dd539853b8e03b5c
Folcon <folcon@gmail.com>
2022-01-19 14:06:20 +0000

    Pulled in KdotJPG's OpenSimplex2 implementation
    KdotJPG/OpenSimplex2 is licensed under the Creative Commons Zero v1.0 Universal
    - https://github.com/KdotJPG/OpenSimplex2/blob/e09a94744b3d69e4c58ce615445f18712cb50104/LICENSE

--- src/fruit_economy/gen_land.clj
+++ src/fruit_economy/gen_land.clj
@@ -1,46 +1,78 @@
 (ns fruit-economy.gen-land
-  (:require [fruit-economy.noise :refer [make-terrain make-perlin-noise messy-noise image->number scale v* compose gradient render]]))
+  (:require [fruit-economy.noise :refer [coord->noise simplex-noise-fn]]))
 
-
-(def width 5 #_50)
-(def height 2 #_20)
-
-(def temp-noise
-  [(make-perlin-noise {:seed (inc (rand-int 10000)) :octaves 2})
-   (make-perlin-noise {:seed (inc (rand-int 10000)) :octaves 4})
-   (make-perlin-noise {:seed (inc (rand-int 10000)) :octaves 88})])
-
-(def messy-temp-noise
-  (compose
-    (nth temp-noise 0)
-    (v* (nth temp-noise 1) 0.5)
-    (v* (nth temp-noise 2) 0.25)))
-
-(render (scale (first temp-noise) 4))
-
-(render (apply compose temp-noise))
-(render messy-temp-noise 512)
-(render (fruit-economy.noise/messy-noise-2 temp-noise))
-
-(render (fruit-economy.noise/gradient messy-temp-noise))
-
-(dotimes [_ 10]
-  (messy-noise [2 5] temp-noise))
-
-(messy-noise [2 5] temp-noise)
-(image->number (apply compose temp-noise) 2 5)
-(image->number messy-temp-noise 2 5)
-(image->number (fruit-economy.noise/messy-noise-2 temp-noise) 2 5)
-
-(for [w (range width)
-      h (range height)]
-  [w h])
 
 (comment
-  (let [temp-noise
-        [(make-perlin-noise {:seed 1 :octaves 2})
-         (make-perlin-noise {:seed 2 :octaves 4})
-         (make-perlin-noise {:seed 3 :octaves 88})]
-        [a b c] temp-noise]
-    (render (v* a 1.25) #_(apply compose [(v* a 1.25) (v* b 2) (v* c 4)]))))
+  (let [n 0
+        octaves [2 4 8] lacunarity [1.25 2 4]]
+    (coord->noise simplex-noise-fn [10 10] {:seed 1 :octave (nth octaves n) :lacunarity (nth lacunarity n) :persistence 0.5 :scale [1 1] :normalise? false})))
 
+(comment
+  (let [n 0
+        octaves [2 4 8] lacunarity [1 1.25 2 4]]
+    (coord->noise simplex-noise-fn [10 10] {:seed 1 :octave (nth octaves n) :lacunarity (nth lacunarity n) :persistence 0.5 :scale [1 1] :normalise? true})))
+
+
+;; TODO: Turn these into record based protocol like thing,
+;;   ie you have a noise-map, and call [x y] into it and it does the computation with the values it has
+(defn simplex-noise-map [width height {:keys [seed octave scale low high] :or {scale 1 low 0 high 1}}]
+  (let [norm (fn [v size] (/ (float v) size))]
+    (into []
+      (for [y (range height)]
+        (into []
+          (for [x (range width)]
+            (coord->noise simplex-noise-fn [(norm x width) (norm y height)]
+              {:seed seed :octave octave :lacunarity 1
+               :persistence 0.5 :scale [scale scale]
+               :normalise? true :low -1 :high 1})))))))
+
+(comment
+  (simplex-noise-map 2 3 {:seed 1 :octave 1}))
+
+(defn grey-noise-map
+  "greyscale map"
+  [width height {:keys [seed octaves]}]
+  (let [norm (fn [v size] (/ (float v) size))]
+    (into []
+      (for [y (range height)]
+        (into []
+          (for [x (range width)]
+            (int (* 256 (coord->noise simplex-noise-fn [(norm x width) (norm y height)]
+                          {:seed seed :octave octaves :lacunarity 1
+                           :persistence 0.5 :scale [0.007 0.007]
+                           :normalise? true :low 0 :high 1})))))))))
+
+(defn make-noise-map [width height seed-size octaves]
+  (let [[o0 o1 o2] octaves]
+    {:width width :height height
+     :noise-coll
+     [(simplex-noise-map width height {:seed (inc (rand-int seed-size)) :octave o0})
+      (simplex-noise-map width height {:seed (inc (rand-int seed-size)) :octave o1})
+      (simplex-noise-map width height {:seed (inc (rand-int seed-size)) :octave o2})]}))
+
+(defn make-temp-noise-map [width height]
+  (make-noise-map width height 10000 [2 4 88]))
+
+(comment
+  (make-temp-noise-map 2 3))
+
+(defn make-elev-noise-map [width height]
+  (make-noise-map width height 10000 [4 10 20]))
+
+(defn messy-noise [noise-coll [x y]]
+  (let [[n0 n1 n2] noise-coll]
+    (+
+      (get-in n0 [y x])
+      (* (get-in n1 [y x]) 0.5)
+      (* (get-in n2 [y x]) 0.25))))
+
+(defn process-noise-map [{:keys [width height noise-coll] :as _noise-map} val-mod]
+  (into []
+    (map (fn [row]
+           (mapv (fn [coord]
+                   (+ (messy-noise noise-coll coord) val-mod))
+             row)))
+    (for [y (range height)]
+      (into []
+        (for [x (range width)]
+          [x y])))))
--- src/fruit_economy/noise.clj
+++ src/fruit_economy/noise.clj
@@ -1,159 +1,38 @@
 (ns fruit-economy.noise
-  (:require [clisk.core :refer [show image]]
-            [clisk.patterns :as patterns]
-            [clisk.functions :as functions]
-            [clisk.live :as clisk]
-            [clisk.util :refer [red-from-argb green-from-argb blue-from-argb alpha-from-argb]]))
+  (:import [org.kdotjpg.noise OpenSimplex2]))
 
 
+(defn simplex-noise-fn [seed x y]
+  (OpenSimplex2/noise2 seed x y))
 
-(defn make-perlin-noise [{:keys [seed octaves lacunarity gain scale] :as opts}]
-  (if seed
-    (patterns/seed-perlin-noise! seed)
-    (patterns/seed-perlin-noise!))
-  (image
-    (patterns/make-multi-fractal
-      patterns/perlin-noise
-      opts)
-    opts))
+;; https://cmaher.github.io/posts/working-with-simplex-noise/
+(defn coord->noise [noise-fn [x y] {:keys [seed octave lacunarity persistence scale offset low high normalise?] :or {scale [1 1] offset [0 0]}}]
+  (let [[scale-x scale-y] scale
+        [offset-x offset-y] offset
+        offset-x-fn (if (zero? offset-x) identity (partial + offset-x)) offset-y-fn (if (zero? offset-y) identity (partial + offset-y))
+        [result result-amplitude]
+        (loop [result 0
+               result-amplitude 0
+               frequency 1
+               amplitude 1
+               idx 0]
+          (if (< idx octave)
+            (recur
+              (+ result (* (noise-fn seed (* frequency (offset-x-fn x) scale-x) (* frequency (offset-y-fn y) scale-y)) amplitude))
+              (+ result-amplitude amplitude)
+              (* amplitude persistence)
+              (* frequency lacunarity)
+              (inc idx))
+            [result result-amplitude]))]
+    (cond-> result
+      normalise?
+      (as-> $ (/ $ result-amplitude))
 
-(defn make-terrain [{:keys [seeds seed size octaves lacunarity as-img? display-colour? display-gradient?] :or {seed 10 seeds [10 10 10] octaves [2 4 8] lacunarity [1.25 2 4] size 512}}]
-  (let [opts {:size size :seed seed}
-        img-fn (if as-img? (fn [f] (image f :size size)) identity)
-        display-colour-fn (if display-colour? functions/vector-offsets identity)
-        display-gradient-fn (if display-gradient? functions/gradient identity)
-        ;; TODO: Make noise properly care about seed
-        ;seed (hash seeds)
-        [seed-1 seed-2 seed-3] seeds
-        [octave-1 octave-2 octave-3] octaves
-        [lacunarity-1 lacunarity-2 lacunarity-3] lacunarity
-        _ (patterns/seed-perlin-noise! seed-1)
-        a (img-fn
-            (patterns/make-multi-fractal
-              (display-colour-fn patterns/perlin-noise)
-              (assoc opts :octaves octave-1)))
-        _ (patterns/seed-perlin-noise! seed-2)
-        b (img-fn
-            (patterns/make-multi-fractal
-              (display-colour-fn patterns/perlin-noise)
-              (assoc opts :octaves octave-2)))
-        _ (patterns/seed-perlin-noise! seed-3)
-        c (img-fn
-            (patterns/make-multi-fractal
-              (display-colour-fn patterns/perlin-noise)
-              (assoc opts :octaves octave-3)))]
-    (display-gradient-fn (functions/compose (functions/v* a lacunarity-1) (functions/v* lacunarity-2 b) (functions/v* lacunarity-3 c)))))
-
-(comment
-  (show (functions/height-normal 0.25 (make-terrain {:seed 10 :seeds [10 10 10] :display-colour? true :display-gradient? false :as-img? false})))
-
-  (show (make-terrain {:seed 10 :seeds [10 10 10] :display-colour? true :display-gradient? false :as-img? false}) :size 512)
-
-  (def a (patterns/make-multi-fractal (make-perlin-noise {:size 512})
-           {:octaves 1 :lacunarity 0.5}))
-
-  (def b (patterns/make-multi-fractal (make-perlin-noise {:size 512})
-           {:octaves 1 :lacunarity 0.25}))
-
-  (show b :size 512)
-
-  (let [size 512
-        opts {:size size :seed 10}
-        a (patterns/make-multi-fractal
-            (functions/vector-offsets patterns/perlin-noise)
-            (assoc opts :octaves 2))
-        b (patterns/make-multi-fractal
-            (functions/vector-offsets patterns/perlin-noise)
-            (assoc opts :octaves 4))
-        c (patterns/make-multi-fractal
-            (functions/vector-offsets patterns/perlin-noise)
-            (assoc opts :octaves 8))]
-    (show (functions/gradient (functions/compose (functions/v* a 1.25) (functions/v* 2 b) (functions/v* 4 c))) #_(functions/compose a (functions/v* 2 b) (functions/v* 4 c)) :size size)
-    #_(show
-        (functions/compose
-          a
-          (patterns/make-multi-fractal b {:octaves 2 :lacunarity 1 :gain 0.5 :scale 2})
-          (patterns/make-multi-fractal c {:octaves 1 :lacunarity 1 :gain 0.5 :scale 8}))
-        :size size))
-
-  (show (patterns/make-multi-fractal (make-perlin-noise {:size 512})
-          {:octaves 1 :lacunarity 0.25}) :size 512))
-
-(defn image->rgba [img x y]
-  (let [colour (.getRGB img x y)]
-    (mapv (fn [f] (f colour)) [red-from-argb green-from-argb blue-from-argb alpha-from-argb])))
-
-(defn image->rgb [img x y]
-  (let [colour (.getRGB img x y)]
-    (mapv (fn [f] (f colour)) [red-from-argb green-from-argb blue-from-argb])))
-
-(defn image->number [img x y]
-  (let [colour (.getRGB img x y)]
-    (reduce (fn [v f] (+ v (f colour))) 0 [red-from-argb green-from-argb blue-from-argb])))
-
-
-(defn scale [img-or-fn n]
-  (functions/scale n img-or-fn))
-
-(defn v* [img-or-fn n]
-  (functions/v* img-or-fn n))
-
-(defn compose
-  ([f g]
-   (image (functions/compose f g)))
-  ([f g & more]
-   (image (functions/compose f (apply functions/compose g more)))))
-
-(defn gradient [img-or-fn]
-  (functions/gradient img-or-fn))
-
-(defn render
-  ([img-or-fn]
-   (show img-or-fn))
-  ([img-or-fn size]
-   (show img-or-fn :size size :width size :height size)))
+      (and low high)
+      (as-> $ (+ (/ (* $ (- high low)) 2) (/ (+ high low) 2))))))
 
 
 (comment
-  (show (functions/seamless 0.25 (compose patterns/plasma patterns/vsnoise)) :size 512))
-
-
-(defn messy-noise
-  "nicely scales the noise using the standard notion of lacunarity"
-  [[x y] noise-coll]
-  (let [[n0 n1 n2] noise-coll]
-    (+
-      (image->number n0 x y)
-      (* 0.5 (image->number n1 x y))
-      (* 0.25 (image->number n2 x y)))))
-
-(defn messy-noise-2
-  "nicely scales the noise using the standard notion of lacunarity"
-  [[n0 n1 n2]]
-  (compose
-     n0
-     (patterns/make-multi-fractal n1 {:octaves 1 :lacunarity 1 :gain 1 :scale 0.5})
-     (patterns/make-multi-fractal n2 {:octaves 1 :lacunarity 1 :gain 1 :scale 0.25})))
-
-
-(comment
-  (def noise-1 (make-perlin-noise {:seed (inc (rand-int 10000)) :octaves 2}))
-  (def noise-2 (make-perlin-noise {:seed 20 :octaves 2}))
-
-  (def seed (reduce + (map (comp int char) "Fruit")))
-  (patterns/seed-perlin-noise! seed)
-  (patterns/seed-simplex-noise! seed)
-
-  (def noise-1 (make-perlin-noise {:seed 10 :octaves 2}))
-  (def noise-2 (make-perlin-noise {:seed 20 :octaves 2}))
-
-  (show noise-1)
-  (show noise-2)
-
-  (functions/evaluate (functions/texture-map noise-1))
-
-  (mapv (fn [f] (f (.getRGB noise-1 100 0))) [red-from-argb green-from-argb blue-from-argb alpha-from-argb])
-
-  (show [functions/y functions/y 0])
-
-  (clisk.node/node [functions/x functions/y 0]))
+  (let [n 0
+        octaves [2 4 8] lacunarity [1.25 2 4]]
+    (coord->noise simplex-noise-fn [10 10] {:seed 1 :octave (nth octaves n) :lacunarity (nth lacunarity n) :persistence 0.5 :scale [1 1] :normalise? false})))
