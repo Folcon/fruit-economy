83cd4f379c1f1c5b7d9b643e4064a9f16f68924c
Folcon <folcon@gmail.com>
2022-01-21 19:21:41 +0000

    Render units on top, so they don't get hidden by territory

--- src/fruit_economy/core.clj
+++ src/fruit_economy/core.clj
@@ -155,8 +155,8 @@
                   territory? (contains? territory loc)
                   {::civ/keys [symbol tint] :as civ} (get civ-name->civ (get area->civ-name loc))
                   [glyph tile-colour font dx dy] (cond
-                                                   civ [symbol tint font-default 0 cell]
                                                    unit [unit (if territory? tint (land/render-tile-colour tile)) emoji-font emoji-offset-x emoji-offset-y]
+                                                   civ [symbol tint font-default 0 cell]
                                                    territory? ["" #_(land/render-tile-str tile) tint font-default 0 cell]
                                                    :else ["" #_(land/render-tile-str tile) (land/render-tile-colour tile) font-default 0 cell])
                   tile-colour (if (= (:world hovering) loc) (colour 255 255 255) tile-colour)
