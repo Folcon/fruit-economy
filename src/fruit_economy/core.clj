26e49db1d8f90ae8940b1afaf52be075cce70944
Folcon <folcon@gmail.com>
2022-01-21 15:08:16 +0000

    Stop rendering text to indicate terrain

--- src/fruit_economy/core.clj
+++ src/fruit_economy/core.clj
@@ -157,8 +157,8 @@
                   [glyph tile-colour font dx dy] (cond
                                                    civ [symbol tint font-default 0 cell]
                                                    unit [unit (if territory? tint (land/render-tile-colour tile)) emoji-font emoji-offset-x emoji-offset-y]
-                                                   territory? [(land/render-tile-str tile) tint font-default 0 cell]
-                                                   :else [(land/render-tile-str tile) (land/render-tile-colour tile) font-default 0 cell])
+                                                   territory? ["" #_(land/render-tile-str tile) tint font-default 0 cell]
+                                                   :else ["" #_(land/render-tile-str tile) (land/render-tile-colour tile) font-default 0 cell])
                   tile-colour (if (= (:world hovering) loc) (colour 255 255 255) tile-colour)
                   fill (doto (Paint.) (.setColor tile-colour))]]
       (.drawRect canvas (Rect/makeXYWH (* x cell) (* y cell) cell cell) fill)
