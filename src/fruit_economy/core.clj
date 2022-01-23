d13eef94ebe0ab22132cb8e3f824eb23f20c1702
Folcon <folcon@gmail.com>
2022-01-23 18:42:41 +0000

    Actually use the thread macro that's there...

--- src/fruit_economy/core.clj
+++ src/fruit_economy/core.clj
@@ -35,7 +35,9 @@
      :peep [5 5]
      :cell 20
 
-     :world (-> (economy/add-resources (land/populate (land/gen-land (land/make-land "World" width height)) 100))
+     :world (-> (land/gen-land (land/make-land "World" width height))
+              (land/populate 100)
+              (economy/add-resources)
               (civ/try-spawn-new-civs 10))
      :history-index 0
      :civ-index 0
