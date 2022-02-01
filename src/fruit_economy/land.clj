b6723a45df772010ad9b12f1d0438bb561b2db98
Folcon <folcon@gmail.com>
2022-02-01 22:56:28 +0000

    Added random unit movement
    Should change the units to use an entity system and I don't like the fact that it returns a map-entry, but for now it's ok.

--- src/fruit_economy/land.clj
+++ src/fruit_economy/land.clj
@@ -125,6 +125,14 @@
     ;; basically try three times for each
     (range (* n 3))))
 
+(defn unit-on-tick [{:keys [loc] :as unit} land-data]
+  (let [[x y] loc dir-x (rand-nth [-1 0 1]) dir-y (rand-nth [-1 0 1])
+        x' (+ x dir-x) y' (+ y dir-y) loc' [x' y']
+        target (get-in land-data [::terrain y' x'])]
+    (if (not= target :ocean)
+      [loc' (assoc unit :loc loc')]
+      [loc unit])))
+
 (defn spawn-units [{::keys [width height] :as land-data} n]
   (reduce
     (fn [land attempt]
@@ -144,7 +152,7 @@
                                          [:dragon :monster "🐉"]
                                          [:spider :monster "🕷️"]])]
             (println sub-kind kind glyph)
-            (assoc-in land [::area->units [x y]] {:name (str (name target) "-" (name sub-kind)) :kind kind :glyph glyph}))
+            (assoc-in land [::area->units [x y]] {:name (str (name target) "-" (name sub-kind)) :kind kind :glyph glyph :loc [x y] :on-tick unit-on-tick}))
 
           :else
           land)))
