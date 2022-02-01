2c179c321cfb21563bf289b8cee51a63d45551b4
Folcon <folcon@gmail.com>
2022-02-01 22:51:35 +0000

    add tick handling for units

--- src/fruit_economy/game.clj
+++ src/fruit_economy/game.clj
@@ -1,11 +1,18 @@
 (ns fruit-economy.game
   (:require [fruit-economy.rand :as rand]
+            [fruit-economy.land :as land]
             [fruit-economy.civ :as civ]))
 
 
+(defn unit-tick [land-data]
+  (println :unit-tick)
+  (-> land-data
+    (update ::land/area->units #(into {} (map (fn [[area {:keys [on-tick] :as unit}]] (if on-tick (on-tick unit land-data) unit))) %))))
+
 (defn land-tick [land-data]
   (println :land-tick)
-  land-data)
+  (-> land-data
+    (unit-tick)))
 
 (defn on-tick [land-data]
   (println :on-tick)
