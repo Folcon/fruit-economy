b24416386455b24c7bcbdc7fcd70b7c5a3261d60
Folcon <folcon@gmail.com>
2022-02-06 15:28:09 +0000

    Created a notion of decisions and sub-decisions
    Just a straight rewrite of the idea splitting things into high level *decisions* and sub-decisions called *choices*.
    
    Need to do a cleanup of the print code.
    
    There's already an issue with some sub-decisions not being possible, so the decisions should have been eliminated earlier, for example deciding to claim territory, except the only territory available to claim is ocean terrain, and the civ can't claim that. Basically should the decision have been made in the first place? From a this is a bug perspective, I was originally thinking that this should have been eliminated earlier in the chain, ie high level agents should remove invalid decisions from the pool of candidates and I still believe that is the case for now, but it raises an interesting thing, which is two possible additional behaviours, leaving that in means you can now have additional depth at both levels:
    - Leaders from micromanagers who eliminate choices giving their reports only a small subset of the available options to careful leaders who just remove obviously bad options to thoughtless leaders who just say get it done, giving free reign to their subordinates.
    - Space for the subordinates to simply make a choice from the options available to them, say none of the options are valid giving and just throwing their arms up saying nothing to be done here, because they don't like any of the options or they want to block progress for a turn, to subversively selecting a valid option which was eliminated earlier by their boss.

--- src/fruit_economy/sim/core.clj
+++ src/fruit_economy/sim/core.clj
@@ -5,6 +5,51 @@
             [fruit-economy.sim.coords :as coords]))
 
 
+;; The decision system, basically actions are broken down into decisions and choices, at the decision stage we're
+;;   picking between a bunch of options, eliminating any ones we can because we know we can't follow through with it,
+;;   because we don't meet a prerequisite.
+;;   Once a decision is decided, we then need to make some sub-decisions which for now we'll call choices.
+;;   These choices are for figuring out details, for example: if we decide to claim a territory,
+;;   the choice would be which one?
+(defn decide
+  "Makes a high level decision, basically what will be done"
+  [land-data {:keys [decisions name] :as peep}]
+  ;; Eliminate options, for example are there valid territories to claim? If not we shouldn't choose it,
+  ;;   there's scope in the future when we weight decisions based on the peep where if they can't choose their favourite
+  ;;   decision it annoys them or makes them brood for a turn etc...
+  {:decision (rand-nth decisions) :decider name})
+
+(defn choose
+  "Decides the details of the decision given, these sub-decisions won't necessarily be made by the same peep who made
+   the decision."
+  [{::land/keys [terrain] :as land-data} {civ-name :civ/name :keys [name] :as peep} {decision-action :decision :as decision}]
+  (let [territory (get-in land-data [::land/civ-name->civ civ-name ::civ/territory])
+        _ (println :territory territory)]
+    (condp = decision-action
+      :claim (let [candidates (coords/get-territory-borders land-data territory)
+                   ;; remove invalid, locations TODO: can we turn this into a xf and pass optionally pass it into get-territory-borders?
+                   _ (println :claim :pre-candidates candidates terrain)
+                   candidates (into [] (remove (fn [[x y]] (= (get-in terrain [y x]) :ocean))) candidates)
+                   _ (println :claim :candidates candidates)
+                   target (rand-nth candidates)]
+               ;; we get a sub-decision to attempt to claim target!
+               (assoc decision :target target :chooser name))
+      :develop (let [candidates (vec territory)
+                     _ (println :develop :candidates candidates)
+                     target (rand-nth candidates)]
+                 ;; we get a sub-decision to attempt to develop (reduce the wildness of the tile or make the tile more
+                 ;;   hospitable to the civ, so maybe wild races make the tiles wilder?) target!
+                 (assoc decision :target target :chooser name))
+      :gather (let [;; This one may need a precondition that the civ has a claimed resource?
+                    area->resources (get land-data ::land/area->resources)
+                    candidates (into [] (filter area->resources) territory)
+                    _ (println :gather :candidates candidates)
+                    target (rand-nth candidates)]
+                ;; we get a sub-decision to attempt to gather target resource!
+                (assoc decision :target target :chooser name))
+      ;; no real sub-decision to be made here, we just have the pop grow
+      :grow decision)))
+
 (comment
   (let [width 3 height 3
         top-left-x 0 top-left-y 0
