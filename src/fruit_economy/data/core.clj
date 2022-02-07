c367af5a54b3810a32d297d4a067abf7cba5bdde
Folcon <folcon@gmail.com>
2022-02-07 22:04:10 +0000

    Add `log-history`, `upsert-civ` and `update-civ`

--- src/fruit_economy/data/core.clj
+++ src/fruit_economy/data/core.clj
@@ -6,6 +6,21 @@
 (defn land-data [world-db]
   (db/q '[:find (pull ?e [*]) . :where [?e :fruit-economy.land/terrain]] world-db))
 
+(defn log-history [world-db message]
+  (let [[id history] (db/q '[:find [?e ?value] :where [?e :fruit-economy.land/history ?value]] world-db)]
+    (db/db-bulk-insert world-db
+      [{:db/id id :fruit-economy.land/history (conj history message)}])))
+
 (defn history-log-entries [world-db]
   (db/q '[:find ?value . :where [?e :fruit-economy.land/history ?value]] world-db))
+
+(defn upsert-civ [world-db civ-name attrs]
+  (let [civ-id (db/q '[:find ?e . :where [?e :fruit-economy.civ/name ?civ-name] :in $ ?civ-name] world-db civ-name)]
+    (db/db-bulk-insert world-db
+      [(assoc attrs :db/id civ-id)])))
+
+(defn update-civ [world-db civ-name attrs]
+  (let [ent (db/q '[:find (pull ?e ?attrs) . :where [?e ::civ/name ?civ-name] :in $ ?civ-name ?attrs] world-db civ-name (into [:db/id] (keys attrs)))]
+    (db/db-bulk-insert world-db
+      [(reduce-kv (fn [e k f] (update e k f)) ent attrs)])))
 
