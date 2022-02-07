a377702188db48655fbc2cb592b1c66deac06918
Folcon <folcon@gmail.com>
2022-02-07 15:55:45 +0000

    Fixed ref handling, no more dangling refs

--- src/fruit_economy/db/core.clj
+++ src/fruit_economy/db/core.clj
@@ -10,13 +10,31 @@
 ; [kind id {attrs values}]
 
 (defn db-insert [db [_op kind id attr-map]]
-  (assoc-in db [kind id] attr-map))
+  (let [refs (into [] (remove (fn [[_k v]] (not (vector? v)))) attr-map)]
+    (-> db
+      (assoc-in [:data kind id] attr-map)
+      (as-> $
+        (reduce
+          (fn [db [foreign-key [k v]]]
+            (let [ref [kind id foreign-key]]
+              (update-in db [:refs k v] (fnil conj #{}) ref)))
+          $
+          refs)))))
 
 (defn db-update [db [_op kind id attr f & args]]
-  (apply update-in db [kind id attr] f args))
+  (apply update-in db [:data kind id attr] f args))
 
 (defn db-delete [db [_op kind id]]
-  (update db kind dissoc id))
+  (let [refs (get-in db [:refs kind id])]
+    (cond-> (update-in db [:data kind] dissoc id)
+      refs
+      (as-> $
+        (reduce
+          (fn [db [kind id foreign-key]]
+            (update-in db [:data kind id] dissoc foreign-key))
+          $
+          refs)))))
+
 
 (comment
   (let [ops [[:insert :civ 1 {:name "Civ 1" :peeps [:peep 2] :power 10}]
