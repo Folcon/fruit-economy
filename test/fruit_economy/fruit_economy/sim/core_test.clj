4571e61e65b6aff5218e39827d77caed0e0b34c7
Folcon <folcon@gmail.com>
2022-02-07 18:06:26 +0000

    Adding a test case to check dumping civ-data

--- test/fruit_economy/fruit_economy/sim/core_test.clj
+++ test/fruit_economy/fruit_economy/sim/core_test.clj
@@ -50,6 +50,43 @@
                  db))
             0)))))
 
+(deftest adding-civ-data-to-db-test
+  (let [width 1 height 1
+        world-name "World"
+        land-data (-> (land/make-land world-name width height)
+                    (land/gen-land))
+
+        {::land/keys [name terrain curr-civ-id civ-letters lang] :as land-data} land-data
+        x (rand-int width) y (rand-int height)
+        civ-loc [x y]
+        civ-path [y x]
+        parent-civ nil
+        symbol (first civ-letters)
+        civ-name (lang/make-word lang)
+        biome (get-in terrain civ-path)
+        new-civ (civ/make-civ curr-civ-id civ-name symbol civ-loc name biome parent-civ [])
+
+        db (-> (init-db)
+             (db-bulk-insert [(assoc land-data :db/id -1)
+                              (assoc new-civ :db/id -2)
+                              [:db/add -1 :land/civs -2]]))]
+    (testing "Testing that I can query out civ data after dumping it into the db"
+      (is (= (ffirst
+               (q '[:find ?value
+                    :where [?e ::civ/name ?value]]
+                 db))
+            civ-name))
+      (is (= (ffirst
+               (q '[:find ?value
+                    :where [?e ::civ/symbol ?value]]
+                 db))
+            symbol))
+      (is (= (ffirst
+               (q '[:find ?value
+                    :where [?e ::civ/home-biome ?value]]
+                 db))
+            biome)))))
+
 
 (comment
   ;; So we now have a sort of API that we'd like to use and this obviously doesn't work
