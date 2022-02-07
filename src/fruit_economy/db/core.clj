e73e9a618c1e26fdfb4bbf96ff62c4ab4edb4bf1
Folcon <folcon@gmail.com>
2022-02-07 15:29:04 +0000

    Add `:update` and pull out `op` functions

--- src/fruit_economy/db/core.clj
+++ src/fruit_economy/db/core.clj
@@ -9,13 +9,20 @@
 ;;;   and update a denormalised map of paths
 ; [kind id {attrs values}]
 
-(comment
-  (let [ops [[:insert :civ 1 {:name "Civ 1" :peeps [:peep 2]}]
-             [:insert :peep 2 {:name "Peep 1" :civ [:civ 1]}]]]
-    (reduce
-      (fn [db [op kind id attr-map]]
-        (condp = op
-          :insert (assoc-in db [kind id] attr-map)))
+(defn db-insert [db [_op kind id attr-map]]
+  (assoc-in db [kind id] attr-map))
 
+(defn db-update [db [_op kind id attr f & args]]
+  (apply update-in db [kind id attr] f args))
+
+(comment
+  (let [ops [[:insert :civ 1 {:name "Civ 1" :peeps [:peep 2] :power 10}]
+             [:insert :peep 2 {:name "Peep 1" :civ [:civ 1]}]
+             [:update :civ 1 :power inc]]]
+    (reduce
+      (fn [db op]
+        (condp = (first op)
+          :insert (db-insert db op)
+          :update (db-update db op)))
       {}
       ops)))
