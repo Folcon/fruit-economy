beb0496080d40a428fcece2213a9bf93f5e2a24d
Folcon <folcon@gmail.com>
2022-02-13 19:35:42 +0000

    Forgot about using `meta` to store `:arglists` and `:doc`, fixed `db/q`

--- src/fruit_economy/db/core.clj
+++ src/fruit_economy/db/core.clj
@@ -51,16 +51,18 @@
     db
     ops))
 
-(def q
-  "[query & inputs]
-   Executes a datalog query. See [docs.datomic.com/on-prem/query.html](https://docs.datomic.com/on-prem/query.html).
-   Usage:
-   ```
-   (q '[:find ?value
-        :where [_ :likes ?value]]
-      db)
-   ; => #{[\"fries\"] [\"candy\"] [\"pie\"] [\"pizza\"]}
-   ```"
+(def ^{:arglists '([query & inputs])
+       :doc "Executes a datalog query. See [docs.datomic.com/on-prem/query.html](https://docs.datomic.com/on-prem/query.html).
+
+          Usage:
+
+          ```
+          (q '[:find ?value
+               :where [_ :likes ?value]]
+             db)
+          ; => #{[\"fries\"] [\"candy\"] [\"pie\"] [\"pizza\"]}
+          ```"}
+  q
   d/q)
 
 (defn init-db []
