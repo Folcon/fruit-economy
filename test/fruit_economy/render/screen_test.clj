932c4220a3348384661674596fafdbb78e2fc22a
Folcon <folcon@gmail.com>
2022-02-22 22:42:20 +0000

    Got cached rendering working it seems
    Commenting out `args` and `args'` in `(println "IN memoize-last" #_args #_args' (= args args'))` as we're currently passing the world in.
    
    We probably need to figure out what we should be passing in

--- test/fruit_economy/render/screen_test.clj
+++ test/fruit_economy/render/screen_test.clj
@@ -6,7 +6,8 @@
             [fruit-economy.land :as land]
             [fruit-economy.core :as core])
   (:import [io.github.humbleui.skija Surface Canvas Color4f FontMgr FontStyle Typeface Font Paint])
-  (:import [io.github.humbleui.types IPoint IRect Rect]))
+  (:import [io.github.humbleui.types IPoint IRect Rect]
+           [java.lang AutoCloseable]))
 
 
 (def world
@@ -31,6 +32,22 @@
 
         world-db (db/db-bulk-insert db [world])]
     world-db))
+
+(def *render-cache (atom {}))
+(defn memoize-last [ctor]
+  (let [*atom (volatile! nil)]
+    (fn [& args']
+      (or
+        (when-some [[args value] @*atom]
+          (println "IN memoize-last" #_args #_args' (= args args'))
+          (if (= args args')
+            value
+            (when (instance? AutoCloseable value)
+              (.close ^AutoCloseable value))))
+        (let [value' (apply ctor args')]
+          (vreset! *atom [args' value'])
+          value')))))
+
 
 (comment
   (let [zoom 1.0
@@ -64,7 +81,11 @@
 
         buffer (Surface/makeRasterN32Premul window-width window-height)
         canvas (.getCanvas buffer)
-        [camera-x camera-y] [10 12]]
+        [camera-x camera-y] [10 12]
+        make-rendered (fn [cell] (let [buffer (Surface/makeRasterN32Premul cell cell #_#_(* cell 10) (* cell 10))]
+                                   {:buffer buffer
+                                    :canvas (.getCanvas buffer)
+                                    :image (.makeImageSnapshot buffer)}))]
     (println (pr-str terrain))
     (.clear canvas (unchecked-int 0xFFFFFBBB))
     (with-open [w-fill (doto (Paint.) (.setColor (unchecked-int 0xFFCC3333)))
@@ -75,20 +96,32 @@
       (.drawRect canvas viewport-rect v-fill))
 
     (doseq [x (range world-width)
-            y (range world-height)
-            :let [;; pixel-x and pixel-y
-                  px-x (+ (* x cell) viewport-offset) px-y (+ (* y cell) viewport-offset)
+            y (range world-height)]
+      (let [;; args are the args captured in our memoize-last
+            args [world-db x y]
+            render-fn (fn [world-db x y]
+                        (let [_ (println "RERENDERING AT:" x y)
+                              rendered (get-in @*render-cache [args :rendered] (make-rendered cell))
+                              ;; pixel-x and pixel-y
+                              px-x (+ (* x cell) viewport-offset) px-y (+ (* y cell) viewport-offset)
 
-                  loc-x (+ (int (- x half-ww)) camera-x)
-                  loc-y (+ (int (- y half-wh)) camera-y)
-                  loc [loc-x loc-y]
-                  path [loc-y loc-x]
-                  biome (get-in terrain path)
+                              loc-x (+ (int (- x half-ww)) camera-x)
+                              loc-y (+ (int (- y half-wh)) camera-y)
+                              loc [loc-x loc-y]
+                              path [loc-y loc-x]
+                              biome (get-in terrain path)
 
-                  tile-colour (land/render-tile-colour biome)]]
-      (with-open [fill (doto (Paint.) (.setColor tile-colour))]
-        (.drawRect canvas (Rect/makeXYWH px-x px-y cell cell) fill)))
-
+                              tile-colour (land/render-tile-colour biome)]
+                          (with-open [fill (doto (Paint.) (.setColor tile-colour))]
+                            (.drawRect (:canvas rendered) (Rect/makeXYWH 0 0 cell cell) fill))
+                          (assoc rendered :px-x px-x :px-y px-y :image (.makeImageSnapshot (:buffer rendered)))))
+            inputs-fn (get-in @*render-cache [args :fn] (memoize-last render-fn))
+            rendered' (apply inputs-fn args)
+            rendered (get-in @*render-cache [args :rendered])]
+        (when-not (identical? rendered rendered')
+          (swap! *render-cache assoc args {:fn inputs-fn
+                                           :rendered rendered'}))
+        (.drawImage canvas (:image rendered') (:px-x rendered') (:px-y rendered'))))
     (io/copy
       (-> buffer
         (.makeImageSnapshot)
