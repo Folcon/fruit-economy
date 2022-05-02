21b8b914156601777d4c6241f496168aa4a85fc1
Folcon <folcon@gmail.com>
2022-05-02 14:50:26 +0100

    Added support for updates via `:db.fn/call`
    Approach described [here](https://github.com/frankiesardo/minikusari/issues/3).

--- src/fruit_economy/sim/basic.clj
+++ src/fruit_economy/sim/basic.clj
@@ -13,11 +13,14 @@
 (defn rewrite
   "Tries to match rule against db
   Returns tx-data or empty list if rule does not match"
-  [{:keys [when then args]} db]
-  (let [syms (for [row then el row :when (symbol? el)] el)
+  [{:keys [when then args call]} db]
+  (let [ds-var? (fn [sym] (= (first (name sym)) \?))
+        syms (for [row then el row :when (and (symbol? el) (ds-var? el))] el)
         results (apply d/q {:find syms :in (cons '$ (keys args)) :where when} db (vals args))]
     (for [match results tx then]
-      (let [swaps (zipmap syms match)
+      (let [swaps (merge
+                    (zipmap syms match)
+                    call)
             f (fn [x] (if (coll? x) x (get swaps x x)))]
         (walk/postwalk f tx)))))
 
