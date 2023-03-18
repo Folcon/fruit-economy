b8f0644101f14ed0c6feb8026ca05c1bcf8fb13e
Folcon Red <folcon@gmail.com>
2023-03-18 17:48:16 +0000

    Implement rest of `DBOps` behaviour

--- src/fruit_economy/db/datascript_impl.clj
+++ src/fruit_economy/db/datascript_impl.clj
@@ -8,8 +8,22 @@
 (extend-type
   DB
   p/DBOps
-  (query
+  (--query
     ([db query]
      (d/q query db))
     ([db query inputs]
-     (apply d/q query db inputs))))
+     (apply d/q query db inputs)))
+  (--datoms
+    ([db index] (d/datoms db index))
+    ([db index c1] (d/datoms db index c1))
+    ([db index c1 c2] (d/datoms db index c1 c2))
+    ([db index c1 c2 c3] (d/datoms db index c1 c2 c3))
+    ([db index c1 c2 c3 c4] (d/datoms db index c1 c2 c3 c4)))
+  (--entity [db eid] (d/entity db eid))
+  (--touch [e] (d/touch e))
+  (--db-with [db tx-data] (d/db-with db tx-data)))
+
+(defn --empty-db
+  ([] (d/empty-db))
+  ([opts] (d/empty-db))
+  ([schema opts] (d/empty-db schema)))
--- src/fruit_economy/db/protocol.clj
+++ src/fruit_economy/db/protocol.clj
@@ -3,4 +3,8 @@
 
 
 (defprotocol DBOps
-  (query [db query] [db query inputs]))
+  (--query [db query] [db query inputs])
+  (--datoms [db index] [db index c1] [db index c1 c2] [db index c1 c2 c3] [db index c1 c2 c3 c4])
+  (--entity [db eid])
+  (--touch [e])
+  (--db-with [db tx-data]))
